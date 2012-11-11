9b66b6449eec5dd9b2f40751353b3b112f44797e
Jordan Santell <jsantell@gmail.com>
2012-11-10 22:40:53 -0500

    Clean up route formatting

--- routes/packages.js
+++ routes/packages.js
@@ -3,8 +3,11 @@ var handleError = require( '../lib/handleError' );
 // GET /packages
 exports.index = function ( req, res, next ) {
   req.models.Packages.find( {}, function ( err, packages ) {
-    if ( !err ) { res.json( packages ); }
-    else handleError( err, next );
+    if ( !err ) {
+      res.json( packages );
+    } else {
+      handleError( err, next );
+    }
   });
 };
 
@@ -12,18 +15,19 @@ exports.index = function ( req, res, next ) {
 exports.create = function ( req, res, next ) {
   var pkg = new req.models.Packages( req.body );
   pkg.save(function ( err ) {
-    if ( !err ) res.json({ success: true, message: 'Package added' });
-    else handleError( err, next );
+    if ( !err ) {
+      res.json({ success: true, message: 'Package added' });
+    } else {
+      handleError( err, next );
+    }
   });
 };
 
 // GET /packages/:name
 exports.show = function ( req, res, next ) {
   req.models.Packages.findOne({ name: req.params.name }, function ( err, pkg ) {
-    if ( pkg ) {
-      res.json( pkg );
-    } else if ( !err ) {
-      res.json( {} );
+    if ( !err ) {
+      res.json( pkg ? pkg : {}); 
     } else {
       handleError( err, next );
     }
@@ -33,7 +37,10 @@ exports.show = function ( req, res, next ) {
 // GET /packages/search/:name
 exports.search = function ( req, res, next ) {
   req.models.Packages.search( req.params.name, function ( err, packages ) {
-    if ( !err ) { res.json( packages ); }
-    else handleError( err, next );
+    if ( !err ) {
+      res.json( packages );
+    } else {
+      handleError( err, next );
+    }
   });
 };
