71e4e3798f7310124af0e2f08c47a3cd20b9b8b1
Jordan Santell <jsantell@gmail.com>
2012-11-10 22:00:40 -0500

    Add name validation, require script in Package model

--- models/packages.js
+++ models/packages.js
@@ -1,12 +1,16 @@
 module.exports = function ( mongoose ) {
   var Packages = new mongoose.Schema({
-    name : { type: String, required: true, unique: true, index: true },
+    name : { type: String, required: true, unique: true, index: true, lowercase: true },
     repo : { type: String, required: true },
-
+    script : { type: String, required: true },
     description : { type: String, required: true },
     keywords : { type: Array },
     ui : { type: Boolean }
   });
+
+  Packages.path('name').validate(function ( v ) {
+    return /^[a-zA-Z][a-zA-Z0-9\-]{0,29}$/.test( v );
+  }, 'Package names must be between 1 and 30 characters, and contain only letters, numbers and dashes, and must begin with a letter.' );
 
   Packages.statics.search = function ( query, callback ) {
     this.find({}).or([
