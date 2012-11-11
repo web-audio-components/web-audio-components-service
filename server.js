432179aa2e299817275bb18afd7e3b67338573b6
Jordan Santell <jsantell@gmail.com>
2012-11-10 22:00:57 -0500

    Use default 'development' env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var
   express = require( 'express' ),
   mongoose = require( 'mongoose' ),
   reqExtend = require( 'request-extend' ),
-  config = require( './config' )[ process.env.NODE_ENV ],
+  config = require( './config' )[ process.env.NODE_ENV || 'development' ],
   app = express();
 
 var
