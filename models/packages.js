22b826dca9038ad54cf5e673b9b135c58fa31ddf
Jordan Santell <jsantell@gmail.com>
2012-11-15 10:32:13 -0500

    Add iso formatter for Dates, add dummy booleans for instrument/effects

--- models/packages.js
+++ models/packages.js
@@ -3,20 +3,30 @@ module.exports = function ( mongoose ) {
     name : { type: String, required: true, unique: true, index: true, lowercase: true },
     repo : { type: String, required: true },
     script : { type: String, required: true },
+    scriptFile : { type: Buffer, require: true },
     description : { type: String, required: true },
     keywords : { type: Array },
-    ui : { type: Boolean }
+    updated : { type: Date, default: Date.now, set: isoToDate },
+
+    // Not yet implemented
+    ui : { type: Boolean },
+    instrument : { type: Boolean },
+    effect : { type: Boolean }
   });
 
+  // Package name must be between 1 and 30 chars, only letters
+  // numbers, dashes and must begin with a letter
   Packages.path('name').validate(function ( v ) {
     return /^[a-zA-Z][a-zA-Z0-9\-]{0,29}$/.test( v );
   }, 'Package names must be between 1 and 30 characters, and contain only letters, numbers and dashes, and must begin with a letter.' );
 
+  // Repo must be of GitHub repo format "account/reponame"
   Packages.path('repo').validate(function ( v ) {
     return /^[a-zA-Z0-9-_]*\/[a-zA-Z0-9-_]*$/.test( v );
   }, 'Repos must be of the GitHub format "owner/reponame"');
 
   // TODO More efficient querying???
+  // TODO Better matching???
   Packages.statics.search = function ( query, callback ) {
     this.find({}).or([
       { name : { $regex : query, $options : '-i' }},
@@ -26,4 +36,10 @@ module.exports = function ( mongoose ) {
   };
 
   return Packages;
+};
+
+// Takes an ISO6801 string and converts it to a date object
+function isoToDate( s ) {
+  if ( s instanceof Date ) { return s; }
+  if ( typeof s === 'string' ) { return new Date( s ); }
 };
