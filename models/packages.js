7bd136249f8832cd007c96e33f504fc247e5e6ea
Jordan Santell <jsantell@gmail.com>
2012-11-19 00:37:07 -0500

    Add a validation method for testing without the scriptFile

--- models/packages.js
+++ models/packages.js
@@ -1,3 +1,5 @@
+var _ = require( 'underscore' );
+
 module.exports = function ( mongoose ) {
   var Packages = new mongoose.Schema({
     name : { type: String, required: true, unique: true, index: true, lowercase: true },
@@ -34,6 +36,22 @@ module.exports = function ( mongoose ) {
       { description : { $regex : query, $options : '-i' }}
     ]).exec( callback );
   };
+
+  // This checks validity of model minus the scriptFile being present
+  // so we can validate before querying GitHub for the repo's exist and
+  // getting the script
+  Packages.methods.validateWithoutScript = function ( callback ) {
+    this.validate(function ( err ) {
+      callback(
+        ( !err ||
+        _.size( err.errors ) === 1 &&
+        err.errors.scriptFile ) ?
+          false :
+          err
+      );
+    });
+  };
+
 
   return Packages;
 };
