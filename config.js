5260b2bc273711f8d53076156b2d26b6e2092b0c
Jordan Santell <jsantell@gmail.com>
2012-11-18 22:09:33 -0500

    Add GH secret/id in config

--- config.js
+++ config.js
@@ -1,23 +1,28 @@
-module.exports = {
+var config = {
   production : {
     db : {
       URL : process.env['wapm_service_mongodb_url']
     },
-    github_token : process.env['wapm_github_token'],
+    github_client_id : process.env['wapm_github_client_id'],
+    github_client_secret : process.env['wapm_github_client_secret'],
     port : 80
   },
   development : {
     db : {
       URL : 'mongodb://localhost:27017/wapm-service'
     },
-    github_token : process.env['wapm_github_token'],
+    github_client_id : process.env['wapm_github_client_id'],
+    github_client_secret : process.env['wapm_github_client_secret'],
     port : 8000
   },
   test : {
     db : {
       URL : 'mongodb://localhost:27017/wapm-service-test'
     },
-    github_token : process.env['wapm_github_token'],
+    github_client_id : process.env['wapm_github_client_id'],
+    github_client_secret : process.env['wapm_github_client_secret'],
     port : 8000
   } 
 };
+
+module.exports = config[ process.env.NODE_ENV || 'development' ];
