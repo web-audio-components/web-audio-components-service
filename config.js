8302efc0876de46d502184afaa064816448cb6ab
Jordan Santell <jsantell@gmail.com>
2012-11-18 19:07:50 -0500

    Use proper env variable naming, add github token

--- config.js
+++ config.js
@@ -1,20 +1,23 @@
 module.exports = {
   production : {
     db : {
-      URL : process.env['wapm-service-mongodb-url']
+      URL : process.env['wapm_service_mongodb_url']
     },
+    github_token : process.env['wapm_github_token'],
     port : 80
   },
   development : {
     db : {
       URL : 'mongodb://localhost:27017/wapm-service'
     },
+    github_token : process.env['wapm_github_token'],
     port : 8000
   },
   test : {
     db : {
       URL : 'mongodb://localhost:27017/wapm-service-test'
     },
+    github_token : process.env['wapm_github_token'],
     port : 8000
   } 
 };
