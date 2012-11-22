0477085641a060bcb091beee281808b64a153895
Jordan Santell <jsantell@gmail.com>
2012-11-22 14:52:38 -0500

    Mock repos for updating tests

---
+++ test/manifests/update-test-module-1.js
@@ -0,0 +1,10 @@
+{
+  "name" : "update-test-module-1",
+  "description" : "A module used for testing updates from a repository",
+  "repo" : "wapm/update-test-module-1",
+  "keywords" : [
+    "update module",
+    "testing"
+  ],
+  "script" : "update-module-1.js"
+}
---
+++ test/repos/wapm/update-test-module-2/wapm.json
@@ -0,0 +1,10 @@
+{
+  "name" : "update-test-module-2",
+  "description" : "A module used for testing updates from a repository",
+  "repo" : "wapm/update-test-module-2",
+  "keywords" : [
+    "update module",
+    "testing"
+  ],
+  "script" : "update-module-2.js"
+}
