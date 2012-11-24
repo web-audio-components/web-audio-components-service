cf71964f50f390e0cff5ef818022dff283d3729d
Jordan Santell <jsantell@gmail.com>
2012-11-24 17:03:01 -0500

    Force ensureIndexes for Mongo 2.2

--- models.js
+++ models.js
@@ -2,6 +2,13 @@ var
   mongoose = require( 'mongoose' );
   db = mongoose.connection;
  
+var
+  Packages = db.model( 'Packages', require( './models/packages' ));
+
+Packages.ensureIndexes(function ( err ) {
+  console.error(err);
+});
+
 module.exports = {
-  'Packages' : db.model( 'Packages', require( './models/packages' )( mongoose ))
+  'Packages' : Packages
 };
--- models/packages.js
+++ models/packages.js
@@ -1,11 +1,12 @@
-var _ = require( 'underscore' );
+var
+  mongoose = require( 'mongoose' ),
+  _ = require( 'underscore' );
 
-module.exports = function ( mongoose ) {
-  var Packages = new mongoose.Schema({
-    name : { type: String, required: true, index : { unique: true }, lowercase: true },
-    repo : { type: String, required: true },
+var Packages = new mongoose.Schema({
+  name : { type: String, required: true, index: true, unique: true, lowercase: true },
+    repo : { type: String, required: true  },
     script : { type: String, required: true },
-    scriptFile : { type: Buffer, require: true },
+    scriptFile : { type: Buffer, required: true },
     description : { type: String, required: true },
     keywords : { type: Array },
     updated : { type: Date, default: Date.now, set: isoToDate },
@@ -15,50 +16,48 @@ module.exports = function ( mongoose ) {
     ui : { type: Boolean },
     instrument : { type: Boolean },
     effect : { type: Boolean }
-  });
+});
 
-  // Package name must be between 1 and 30 chars, only letters
-  // numbers, dashes and must begin with a letter
-  Packages.path('name').validate(function ( v ) {
-    return /^[a-zA-Z][a-zA-Z0-9\-]{0,29}$/.test( v );
-  }, 'Package names must be between 1 and 30 characters, and contain only letters, numbers and dashes, and must begin with a letter.' );
+// Package name must be between 1 and 30 chars, only letters
+// numbers, dashes and must begin with a letter
+Packages.path('name').validate(function ( v ) {
+  return /^[a-zA-Z][a-zA-Z0-9\-]{0,29}$/.test( v );
+}, 'Package names must be between 1 and 30 characters, and contain only letters, numbers and dashes, and must begin with a letter.' );
 
-  // Repo must be of GitHub repo format "account/reponame"
-  Packages.path('repo').validate(function ( v ) {
-    return /^[a-zA-Z0-9-_]*\/[a-zA-Z0-9-_]*$/.test( v );
-  }, 'Repos must be of the GitHub format "owner/reponame"');
+// Repo must be of GitHub repo format "account/reponame"
+Packages.path('repo').validate(function ( v ) {
+  return /^[a-zA-Z0-9-_]*\/[a-zA-Z0-9-_]*$/.test( v );
+}, 'Repos must be of the GitHub format "owner/reponame"');
 
-  // TODO More efficient querying???
-  // TODO Better matching???
-  Packages.statics.search = function ( query, returnFields, callback ) {
-    this.find({}, returnFields).or([
+// TODO More efficient querying???
+// TODO Better matching???
+Packages.statics.search = function ( query, returnFields, callback ) {
+  this.find({}, returnFields).or([
       { name : { $regex : query, $options : '-i' }},
       { keywords : query },
       { description : { $regex : query, $options : '-i' }}
-    ]).exec( callback );
-  };
+      ]).exec( callback );
+};
 
-  // This checks validity of model minus the scriptFile being present
-  // so we can validate before querying GitHub for the repo's exist and
-  // getting the script
-  Packages.methods.validateWithoutScript = function ( callback ) {
-    this.validate(function ( err ) {
-      callback(
-        ( !err ||
+// This checks validity of model minus the scriptFile being present
+// so we can validate before querying GitHub for the repo's exist and
+// getting the script
+Packages.methods.validateWithoutScript = function ( callback ) {
+  this.validate(function ( err ) {
+    callback(
+      ( !err ||
         _.size( err.errors ) === 1 &&
         err.errors.scriptFile ) ?
-          false :
-          err
+      false :
+      err
       );
-    });
-  };
-
-
-  return Packages;
+  });
 };
 
 // Takes an ISO6801 string and converts it to a date object
 function isoToDate( s ) {
   if ( s instanceof Date ) { return s; }
   if ( typeof s === 'string' ) { return new Date( s ); }
-};
+}
+
+module.exports = Packages;
