b76af26ba3afb49790bf6f302968b43caa6b0f94
Jordan Santell <jsantell@gmail.com>
2013-01-23 16:14:19 -0500

    Update naming of model Packages to Component

--- models.js
+++ models.js
@@ -1,14 +1,14 @@
 var
   mongoose = require( 'mongoose' );
   db = mongoose.connection;
- 
-var
-  Packages = db.model( 'Packages', require( './models/packages' ));
 
-Packages.ensureIndexes(function ( err ) {
+var
+  Component = db.model( 'Component', require( './models/component' ));
+
+Component.ensureIndexes(function ( err ) {
   console.error(err);
 });
 
 module.exports = {
-  'Packages' : Packages
+  'Component' : Component
 };
