8c8dabd4fe75f50e2f8a40a5bf7206391e8731b9
Jordan Santell <jsantell@gmail.com>
2013-01-24 18:52:11 -0500

    Mock component lib will now copy originals over to install dir

--- mocks/component.js
+++ mocks/component.js
@@ -1,19 +1,30 @@
 // Sloppy mock for component.install in tests
 
 var
+  utils   = require('../lib/utils'),
+  fs      = require('fs-extra'),
   Emitter = require('events').EventEmitter;
 
-function Package () {
+function Package (repo, version, options) {
+  this.repo = repo;
+  this.version = version;
+  this.options = options;
 }
 
 Package.prototype.__proto__ = Emitter.prototype;
 
 Package.prototype.install = function () {
-  this.emit('end');
+  var options = {
+    src : '../mocks/components/' + this.repo.replace('/', '-');
+    dest : utils.getInstallDir(this.repo);
+  }
+  fs.copy(options, function (err) {
+    this.emit('end');
+  });
 }
 
 module.exports = {
   install : function (repo, version, options) {
-    return new Package();
+    return new Package(repo, version, options);
   }
 };
