0af2935e66a12042ed8ebdfd893bd6464e215f77
Jordan Santell <jsantell@gmail.com>
2013-01-24 13:43:01 -0500

    Set up seeding in development

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
 var
-  express = require( 'express' ),
-  mongoose = require( 'mongoose' ),
-  reqExtend = require( 'request-extend' ),
-  config = require( './config' ),
-  db = mongoose.connect( config.db.URL ),
+  express = require('express'),
+  mongoose = require('mongoose'),
+  config = require('./config'),
+  db = mongoose.connect(config.db.URL),
   app = express(),
-  models = require( './models' );
+  models = require('./models');
 
 app
-  .use( express.bodyParser() )
-  .use( reqExtend( 'models', models ))
-  .use( reqExtend( 'config', config ));
+  .use(express.bodyParser())
 
-require( './router' )( app );
+require('./router')(app);
 
-app.listen( config.port );
+app.listen(config.port);
 
-console.log( 'WAPM listening on port ' + config.port );
+console.log('Web Audio Components Service');
+console.log('   port : ' + config.port);
+console.log('   env  : ' + config.env);
 
-// Seed, run task manager when not testing
-if ( process.env.NODE_ENV !== 'test' ) {
-  require( './lib/seed' );
-  require( './lib/taskManager' );
+// Run task manager when not testing
+if (!config.isTest) {
+  require('./lib/taskManager');
+}
+
+// Seed in development
+if (config.isDevelopment) {
+  require('./lib/seed')(function (err, count) {
+    if (!err) { console.log('Seeded ' + count + ' components'); }
+  });
 }
 
 module.exports = app;
