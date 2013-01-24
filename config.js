b11b8dad7f02c1eb8ff8e237a30932ba372c4ac8
Jordan Santell <jsantell@gmail.com>
2013-01-24 18:55:26 -0500

    Save and store components and build in test dir for tests

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ config = {
       URL : 'mongodb://localhost:27017/wac-service-test'
     },
     componentsURL : 'http://localhost:8001/mock/registry',
-    componentInstallDir : __dirname + '/mocks/components',
+    componentInstallDir : __dirname + '/test/components',
     componentBuildDir : __dirname + '/test/build',
     port : 8001,
     useMocks : true
@@ -41,11 +41,5 @@ config = config[env];
 config.env = env;
 config.isTest = env === 'test';
 config.isDevelopment = env === 'development';
-
-// If in development, using mocks, change componentInstallDir
-// Better way to do this?
-if (config.isDevelopment && config.useMocks) {
-  config.componentInstallDir = __dirname + '/mocks/components';
-}
 
 module.exports = config;
