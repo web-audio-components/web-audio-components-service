b71edbc0347af0c419c080d485e9aa71b552cbfd
Jordan Santell <jsantell@gmail.com>
2013-01-30 00:24:00 -0500

    Expose Dependency model

--- models.js
+++ models.js
@@ -1,14 +1,16 @@
 var
-  mongoose = require('mongoose');
+  mongoose = require('mongoose'),
   db = mongoose.connection;
 
 var
-  Component = db.model('Component', require('./models/component'));
+  Component  = db.model('Component', require('./models/component')),
+  Dependency = db.model('Dependency', require('./models/dependency'));
 
 Component.ensureIndexes(function (err) {
   console.error(err);
 });
 
 module.exports = {
-  'Component' : Component
+  'Component' : Component,
+  'Dependency' : Dependency
 };
