602af3dd951df028bfb08495692e5731ba5ffb95
Jordan Santell <jsantell@gmail.com>
2013-06-25 00:19:30 -0700

    Rename component helper test

--- test/component-utils.test.js
+++ test/component-utils.test.js
@@ -10,7 +10,7 @@ var
   installDir = config.componentInstallDir + '/',
   utils      = require('../lib/utils'),
   clear      = require('./helpers/clear'),
-  helper     = require('../lib/componentHelper');
+  helper     = require('../lib/component-utils');
 
 var
   overdrive  = require('../mocks/components/web-audio-components-overdrive/component.json'),
@@ -24,8 +24,7 @@ describe('Component Helper', function () {
 
   describe('Install', function () {
     it('should pull down the component.json', function (done) {
-      helper.install(overdrive, function (err) {
-        expect(err).to.not.be.ok;
+      helper.install(overdrive).then(function () {
         fs.exists(utils.getInstallDir(overdrive.repo) + 'component.json', function (exists) {
           expect(exists).to.be.ok;
           done();
@@ -36,21 +35,19 @@ describe('Component Helper', function () {
 
   describe('Build', function () {
     it('should build component and return the built js string in callback', function (done) {
-      helper.install(delay, function (err) {
-        helper.build(delay, function (err, js) {
-          expect(err).to.not.be.ok;
-          expect(js).to.equal(builtDelay);
-          done();
-        });
+      helper.install(delay).then(function () {
+        return helper.build(delay);
+      }).then(function (js) {
+        expect(js).to.equal(builtDelay);
+        done();
       });
     });
     it('returns an error if invalid repo', function (done) {
-      helper.install(delay, function (err) {
-        helper.build({ repo: 'not/real', version: '*' }, function (err, js) {
-          expect(err).to.be.ok;
-          expect(js).to.not.be.ok;
-          done();
-        });
+      helper.install(delay).then(function () {
+        return helper.build({ repo: 'not/real', version: '*' });
+      }).then(null, function (err) {
+        expect(err).to.be.ok;
+        done();
       });
     });
   });
